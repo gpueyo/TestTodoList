adfc53faf59d3e4c440be569729f013fdcf841e5
gpueyo <gpueyo95@gmail.com>
2021-08-01 19:55:55 +0200

    Add tasks and list them

--- todolist/src/store.js
+++ todolist/src/store.js
@@ -4,6 +4,7 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const state = {
+    tasks: {},
 }
 
 export default new Vuex.Store({
@@ -11,10 +12,37 @@ export default new Vuex.Store({
     modules: {
     },
     getters: {
+        getTasks: (state, getters, rootState) => {
+            return state.tasks;
+        },    
     },
     actions: {
+        loadTasks (context, params) {
+            var tasks = localStorage.tasks != undefined ? JSON.parse( localStorage.tasks ) : {};
+            context.commit('setTasks', { data: tasks });
+        },
+
+        addTask (context, params) {
+            if ( params.desc ){
+                var tasks = context.state.tasks;
+                
+                tasks [ new Date().getTime() ] = {
+                    description: params.desc,
+                    checked: false
+                };
+                localStorage.setItem('tasks', JSON.stringify( tasks ));
+                                
+                context.commit('setTasks', { data: tasks });
+            }
+        },
+    
     },
     mutations: {
+        setTasks(state, params) {
+            let values = params.data ? params.data : {};
+            Vue.set(state, 'tasks', { ...values });
+        },
+
     }
 })
 
--- todolist/src/views/Home.vue
+++ todolist/src/views/Home.vue
@@ -1,15 +1,28 @@
 <template>
     <div id="content" class="homepage">
-        <template v-if="Object.keys(tasks).length > 0">
-            <ul>
-                <li v-for="task in tasks">
-                    {{description}}
-                </li>
-            </ul>
-        </template>
-        <template v-else>
-            <p>¡Todo completado! No hay más tareas</p>
-        </template>
+        <div class="task-create">
+            <div class="task">
+                <input type="text" placeholder="Añade una nueva tarea..." v-model="newTask" @keyup.enter="addTask"/>
+                <button @click="addTask">+</button>
+            </div>
+
+            <p v-if="errors.emptyTask">La tarea debe contener texto</p>
+        </div>
+
+        <div class="task-list">
+            <template v-if="Object.keys(tasks).length > 0">
+                <ul>
+                    <li v-for="task in tasks">
+                        {{task.description}}
+                    </li>
+                </ul>
+            </template>
+            <template v-else>
+                <!-- TODO -->
+                <p class="msg-empty">¡Todo completado! No hay más tareas</p>
+            </template>
+        </div>
+
     </div>
 </template>
 
@@ -19,22 +32,41 @@
         name: 'Home',
         data() {
             return {
-                tempTasks: {},
+                newTask: "",
+                errors: {
+                    emptyTask: false,
+                },
             }
         },
         computed: {
-            tasks: {
-                get(){
-                    return this.tempTasks;
-                },
-                set(value){
-                    this.tempTasks = value;
-                },
+            tasks (){
+                var result = this.$store.getters['getTasks'];
+                return result ? result : {};
             },
         },
         methods: {
+            // Utils
+            resetErrors(){
+                this.errors.emptyTask = false
+            },
+
+            // Functionalities
+            addTask(){
+                console.log("ADD TASK");
+                var self = this;
+
+                self.resetErrors();
+
+                if ( self.newTask.trim() != "" ){
+                    self.$store.dispatch('addTask', { desc: self.newTask });
+                    self.newTask = "";
+                } else {
+                    self.errors.emptyTask = true
+                }
+            },
         },
         created() {
+            this.$store.dispatch('loadTasks', {});
         }
     }
 
